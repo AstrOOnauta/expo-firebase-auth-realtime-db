64c57ed6b7d59ae92e0a2ab823b7a64643929121
Will <willian.ralf.rn@gmail.com>
2022-08-27 19:22:57 -0300

    feat: firebase config

---
+++ src/database/firebaseConnection.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: 'https://myapp-10762-default-rtdb.firebaseio.com',
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+}
