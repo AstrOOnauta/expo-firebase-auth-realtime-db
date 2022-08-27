56468cb6b6cac98771fa39ef10dd7e9852ba2ebc
Will <willian.ralf.rn@gmail.com>
2022-08-27 12:59:25 -0300

    feat: jest-expo

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.js',
+  ],
+}
+
+export default config
