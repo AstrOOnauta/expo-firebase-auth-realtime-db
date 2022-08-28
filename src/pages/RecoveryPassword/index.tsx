47800806acf180454aae4c47feb584335a025fe0
Will <willian.ralf.rn@gmail.com>
2022-08-28 14:26:44 -0300

    feat: firebase integration with recovery password screen

--- src/pages/RecoveryPassword/index.tsx
+++ src/pages/RecoveryPassword/index.tsx
@@ -1,20 +1,52 @@
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import { initializeApp } from 'firebase/app'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import React, { useState } from 'react'
+import { Alert } from 'react-native'
+
 import BackButton from '../../components/BackButton'
 import Blob from '../../components/Blob'
 import Button from '../../components/Form/Button'
-
 import Input from '../../components/Form/Input'
+import { firebaseConfig } from '../../database/firebaseConnection'
 import { RecoveryPasswordContainer } from './style'
 
 export default function RecoveryPassword() {
+  const [email, setEmail] = useState<string>('')
+
   const navigation = useNavigation()
+
+  const firebase = initializeApp(firebaseConfig)
+  const auth = getAuth(firebase)
+
+  async function handleSubmit() {
+    if (email !== '') {
+      sendPasswordResetEmail(auth, email)
+        .then(() => {
+          Alert.alert('Success', 'Password reset email sent!')
+        })
+        .catch((error) => {
+          Alert.alert(error.code, error.message)
+        })
+    } else {
+      Alert.alert('Fill the email field!')
+    }
+  }
 
   return (
     <RecoveryPasswordContainer>
       <BackButton onPress={() => navigation.navigate('SignIn' as never)} />
-      <Input placeholder="Email" placeholderTextColor="#AAAAAA" />
-      <Button title="SEND RECOVERY EMAIL" type="primary" />
+      <Input
+        placeholder="Email"
+        placeholderTextColor="#AAAAAA"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Button
+        title="SEND RECOVERY EMAIL"
+        type="primary"
+        onPress={handleSubmit}
+      />
       <Blob isDark position="top-left" />
       <Blob isDark position="bottom-right" />
     </RecoveryPasswordContainer>
