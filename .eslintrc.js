0424f09acc6d1fa9a4eb2573591638d82ff1e6f5
Will <willian.ralf.rn@gmail.com>
2022-08-28 14:18:30 -0300

    feat: firebase integration with signup screen

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
     'prettier/prettier': [
       'error',
       {
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,25 +1,80 @@
+import React, { useState } from 'react'
+import { Alert } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth'
+
 import Blob from '../../components/Blob'
 import Button from '../../components/Form/Button'
-
 import Input from '../../components/Form/Input'
+import { firebaseConfig } from '../../database/firebaseConnection'
 import { SignUpContainer } from './style'
 
 export default function SignUp() {
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
   const navigation = useNavigation()
+
+  const firebase = initializeApp(firebaseConfig)
+  const auth = getAuth(firebase)
+
+  async function handleSubmit() {
+    if (name !== '' || email !== '' || password !== '') {
+      await createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          updateProfile(auth.currentUser!, {
+            displayName: name,
+          })
+            .then(() => {
+              Alert.alert('Success', 'User successfully created!')
+
+              setName('')
+              setEmail('')
+              setPassword('')
+
+              navigation.navigate('SignIn' as never)
+            })
+            .catch((error) => {
+              Alert.alert(error.code, error.message)
+            })
+        })
+        .catch((error) => {
+          Alert.alert('Error', error.message)
+        })
+    }
+  }
 
   return (
     <SignUpContainer>
-      <Input placeholder="Name" placeholderTextColor="#AAAAAA" isDark />
-      <Input placeholder="Email" placeholderTextColor="#AAAAAA" isDark />
+      <Input
+        placeholder="Name"
+        placeholderTextColor="#AAAAAA"
+        isDark
+        value={name}
+        onChangeText={setName}
+      />
+      <Input
+        placeholder="Email"
+        placeholderTextColor="#AAAAAA"
+        isDark
+        value={email}
+        onChangeText={setEmail}
+      />
       <Input
         placeholder="Password"
         placeholderTextColor="#AAAAAA"
         isDark
         isPassword
+        value={password}
+        onChangeText={setPassword}
       />
-      <Button title="SIGN UP" type="primary" />
+      <Button title="SIGN UP" type="primary" onPress={handleSubmit} />
       <Button
         title="I HAVE AN ACCOUNT"
         type="secondary"
